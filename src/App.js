980f02ad40fe281eec5e81914830fb3053ab63ef
MarcObanga92 <marc-obanga.fall@ism.edu.sn>
2023-11-07 13:25:46 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        {/* Contenu de votre application */}
+        <h1>Mon application React avec react-bootstrap</h1>
+        <Button variant="primary">Cliquez ici</Button>
+        {/* Ajoutez d'autres éléments ici */}
+      </div>
+    </>
+  );
+}
+
+export default App;
