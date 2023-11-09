784382aaec064bad46c1b5ea02b0721dd63c4e8d
MarcObanga92 <marc-obanga.fall@ism.edu.sn>
2023-11-09 17:50:43 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Card, Button, CardGroup } from 'react-bootstrap';
 
 function App() {
   return (
     <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Mon Projet React</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Accueil</Nav.Link>
+            <Nav.Link href="#features">Fonctionnalités</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
       <div className="App">
-        {/* Contenu de votre application */}
-        <h1>Mon application React avec react-bootstrap</h1>
-        <Button variant="primary">Cliquez ici</Button>
-        {/* Ajoutez d'autres éléments ici */}
+        <header className="App-header">
+          <h1>Bienvenue dans mon application React</h1>
+        </header>
+
+        <CardGroup>
+  <Card>
+    <Card.Img variant="top" src={require('./image1.jpeg').default} />
+    <Card.Body>
+      <Card.Title>Carte 1</Card.Title>
+      <Card.Text>
+        Description de la carte 1.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+
+  <Card>
+    <Card.Img variant="top" src={require('./image2.jpeg').default} />
+    <Card.Body>
+      <Card.Title>Carte 2</Card.Title>
+      <Card.Text>
+        Description de la carte 2.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+
+  <Card>
+    <Card.Img variant="top" src={require('./image3.jpeg').default} />
+    <Card.Body>
+      <Card.Title>Carte 3</Card.Title>
+      <Card.Text>
+        Description de la carte 3.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+</CardGroup>
+
+        <Button variant="primary">Mon Bouton</Button>
       </div>
     </>
   );
